ef9b2aa1a0000d2214e4d05428f8718c0ee93832
prakashvaithianathan <60776039+prakashvaithianathan@users.noreply.github.com>
2021-05-29 11:39:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -202,7 +202,7 @@ const result = async () => {
          })
       });
       document.addEventListener('click',function(){
-        spinnerBorder.style.display="none";
+        spinnerBorder.style.display="flex";
         weatherCon.style.display="none";
       })
     });
